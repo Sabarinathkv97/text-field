3038877cbb9b816f6f360507ab4f30355c44aa64
Sabarinathkv97 <sabarinathkv97@gmail.com>
2023-09-25 13:00:04 +0530

    first commit

---
+++ flutter_application_1/lib/first.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class newapp extends StatefulWidget {
+  const newapp({super.key});
+
+  @override
+  State<newapp> createState() => _newappState();
+}
+
+class _newappState extends State<newapp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(' TextField '),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                      hintText: 'Enter Your Name',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter Password',
+                    ),
+                  ),
+                ),
+                TextButton(
+                  child: Text('Sign In'),
+                  onPressed: () {},
+                )
+              ],
+            )));
+  }
+}
